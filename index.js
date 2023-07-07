db1a41f7eb765eb6cb891a3fca2954504d1f43e1
Samuel Udoh <97406796+Sleek121@users.noreply.github.com>
2023-07-07 18:33:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,105 @@
+const burger = document.querySelector('#burger');
+const cart = document.querySelector('cart');
+const menu = document.querySelector('#menu');
+
+burger.addEventListener('click', () => {
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+    }
+    else { menu.classList.add('hidden');
+}
+})
+
+
+
+
+
+
+
+let count = 0;
+document.getElementById("btn").onclick = function(){
+    count+=1;
+    document.getElementById("countLabel", 'countLabel2').innerHTML = count;
+    alert("One item added to cart!");
+
+    document.getElementById("countLabel2").innerHTML = count;
+    
+}
+
+document.getElementById("btn2").onclick = function(){
+    count+=1;
+    document.getElementById("countLabel", "countLabel2").innerHTML = count;
+    alert("One item added to cart!");
+    document.getElementById("countLabel2").innerHTML = count;
+}
+
+document.getElementById("btn3").onclick = function(){
+    count+=1;
+    document.getElementById("countLabel", "countLabel2").innerHTML = count;
+    alert("One item added to cart!");
+    document.getElementById("countLabel2").innerHTML = count;
+}
+
+document.getElementById("btn4").onclick = function(){
+    count+=1;
+    document.getElementById("countLabel", "countLabel2").innerHTML = count;
+    alert("One item added to cart!");
+    document.getElementById("countLabel2").innerHTML = count;
+}
+
+document.getElementById("btn5").onclick = function(){
+    count+=1;
+    document.getElementById("countLabel", "countLabel2").innerHTML = count;
+    alert("One item added to cart!");
+    document.getElementById("countLabel2").innerHTML = count;
+}
+
+document.getElementById("btn6").onclick = function(){
+    count+=1;
+    document.getElementById("countLabel", "countLabel2").innerHTML = count;
+    alert("One item added to cart!");
+    document.getElementById("countLabel2").innerHTML = count;
+
+}
+
+document.getElementById("btn7").onclick = function(){
+    count+=1;
+    document.getElementById("countLabel", "countLabel2").innerHTML = count;
+    alert("One item added to cart!");
+    document.getElementById("countLabel2").innerHTML = count;
+}
+
+document.getElementById("btn8").onclick = function(){
+    count+=1;
+    document.getElementById("countLabel", "countLabel2").innerHTML = count;
+    alert("One item added to cart!");
+    document.getElementById("countLabel2").innerHTML = count;
+}
+
+document.getElementById("btn9").onclick = function(){
+    count+=1;
+    document.getElementById("countLabel", "countLabel2").innerHTML = count;
+    alert("One item added to cart!");
+    document.getElementById("countLabel2").innerHTML = count;
+}
+
+document.getElementById("btn10").onclick = function(){
+    count+=1;
+    document.getElementById("countLabel", "countLabel2").innerHTML = count;
+    alert("One item added to cart!");
+    document.getElementById("countLabel2").innerHTML = count;
+}
+
+document.getElementById("btn11").onclick = function(){
+    count+=1;
+    document.getElementById("countLabel", "countLabel2").innerHTML = count;
+    alert("One item added to cart!");
+    document.getElementById("countLabel2").innerHTML = count;
+}
+
+document.getElementById("btn12").onclick = function(){
+    count+=1;
+    document.getElementById("countLabel", "countLabel2").innerHTML = count;
+    alert("One item added to cart!");
+    document.getElementById("countLabel2").innerHTML = count;
+}
